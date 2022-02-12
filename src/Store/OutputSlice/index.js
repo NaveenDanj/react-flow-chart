4e13a16ab91c4f9246929f60b08f332f28273e73
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-02-12 10:48:08 +0530

    added output slice

---
+++ src/Store/OutputSlice/index.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  output: [],
+}
+
+export const OutputSlice = createSlice({
+  name: 'Output',
+  initialState,
+  reducers: {
+    
+    resetOutput : (state) => {
+        state.output = [];
+    },
+
+    addOutput : (state , action) => {
+        state.output.push(action.payload);
+    }
+
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { resetOutput , addOutput } = OutputSlice.actions
+export default OutputSlice.reducer
--- src/Store/index.js
+++ src/Store/index.js
@@ -11,11 +11,13 @@ import { configureStore } from '@reduxjs/toolkit'
 //   } from 'redux-persist';
 import nodesReducer from './NodeSlice/index'
 import varsReducer from './ScriptVarObjectSlice/index'
+import OutputSlice from './OutputSlice'
 
 export const store = configureStore({
   reducer: {
     Nodes : nodesReducer,
-    Vars : varsReducer
+    Vars : varsReducer,
+    Output : OutputSlice
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     //     serializableCheck: {
     //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
